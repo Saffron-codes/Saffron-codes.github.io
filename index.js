02df773213f9b711a4f77e7a565b8d9465404c53
Saffron-codes <saffrontdionysius@gmail.com>
2022-01-29 14:39:16 +0530

    navigation

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+if ("serviceWorker" in navigator) {
+    console.log("service worker registered")
+    window.addEventListener("load", function() {
+      navigator.serviceWorker
+        .register("/serviceWorker.js")
+        .then(res => console.log("service worker registered"))
+        .catch(err => console.log("service worker not registered", err))
+    })
+  }
+
 function toggleMobileMenu(menu) {
     menu.classList.toggle('open');
 }
