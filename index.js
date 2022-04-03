e1ffc9c4224c148162ad69925b4b3b6676b54f3a
Saffron-codes <saffrontdionysius@gmail.com>
2022-04-03 20:25:33 +0530

    home page

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+
+var mobileSpin = document.getElementById("mobile");
+console.log(mobileSpin.style.visibility);
+mobileSpin.style.visibility = 'visible';
+console.log(mobileSpin)
+
 function toggleMobileMenu(menu) {
+    // mobileSpin.style.width=0;
     menu.classList.toggle('open');
+    mobileSpin.style.visibility = 'visible';
+console.log(mobileSpin.style.visibility);
+
 }
+
