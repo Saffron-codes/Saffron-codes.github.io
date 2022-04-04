e236c947028a259b8bcbe683ee10dbe92c8205b8
Saffron-codes <saffrontdionysius@gmail.com>
2022-04-04 20:01:50 +0530

    improved ui

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-var mobileSpin = document.getElementById("mobile");
+var mobileSpin = document.querySelector(".welcome img")
 console.log(mobileSpin.style.visibility);
 mobileSpin.style.visibility = 'visible';
 console.log(mobileSpin)
@@ -7,8 +7,19 @@ console.log(mobileSpin)
 function toggleMobileMenu(menu) {
     // mobileSpin.style.width=0;
     menu.classList.toggle('open');
-    mobileSpin.style.visibility = 'visible';
-console.log(mobileSpin.style.visibility);
+    // mobileSpin.style.visibility = 'visible';
+    // console.log(mobileSpin.style.visibility);
+
+
+    if (mobileSpin.style.visibility == 'hidden'){
+        mobileSpin.style.visibility = 'visible';
+        console.log("Visible");
+    }
+        
+    else{
+        mobileSpin.style.visibility = 'hidden';
+        console.log("Hidden");
+    }
 
 }
 
