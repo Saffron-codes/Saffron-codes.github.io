5a9141db2f908e19e2f637c087144d06aed39cb0
Saffron Dionysius <saffrontdionysius@gmail.com>
2022-12-21 13:07:54 +0530

    added routing to new portfolio webpage

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ console.log(mobileSpin.style.visibility);
 mobileSpin.style.visibility = 'visible';
 console.log(mobileSpin)
 
+
 function toggleMobileMenu(menu) {
     // mobileSpin.style.width=0;
     menu.classList.toggle('open');
